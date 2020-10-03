3a3031c87c3082e2951f97754a0d8c4cb4b5c8e5
triplem <160079+triplem@users.noreply.github.com>
2020-10-03 22:09:29 +0200

    force sonar issue

--- src/main/kotlin/org/javafreedom/kcd/service/ValueRecalculations.kt
+++ src/main/kotlin/org/javafreedom/kcd/service/ValueRecalculations.kt
@@ -8,6 +8,9 @@ object ValueRecalculations {
     val recalculations =
         mapOf(
             "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
+//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
+//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
+//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
         )
 
     fun mmol(value: Any?): Any {
