aa2ea612c61e29a3fa17651e349224d4eec88adf
triplem <160079+triplem@users.noreply.github.com>
2020-10-04 19:58:13 +0200

    chore: force sonar issue

--- src/main/kotlin/org/javafreedom/kcd/service/ValueRecalculations.kt
+++ src/main/kotlin/org/javafreedom/kcd/service/ValueRecalculations.kt
@@ -8,18 +8,10 @@ object ValueRecalculations {
     val recalculations =
         mapOf(
             "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
-//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
-//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
-//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
-//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
-//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
-//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
-//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
-//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
-//            "mmol/l" to listOf<(value: Any?) -> Any>(ValueRecalculations::mmol)
         )
 
     fun mmol(value: Any?): Any {
+        println(value.toString())
         return when (value) {
             is Number -> value.toDouble().times(MMOL_TO_MGDL).roundToInt()
             else -> -1
