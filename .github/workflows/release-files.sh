97dda912c12c6cab8644c510216a1da83748a934
triplem <160079+triplem@users.noreply.github.com>
2020-10-06 22:38:28 +0200

    fix: use variable

--- .github/workflows/release-files.sh
+++ .github/workflows/release-files.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 cd ../..
 ./gradlew -Pversion=$1 publish
+<<<<<<< HEAD
 echo "revnumber=$1 >> \$GITHUB_ENV" >> $GITHUB_WORKSPACE/revnumber.sh
+=======
+export revnumber=$1
+>>>>>>> 18adf7c... fix: use variable
