5f2f34a70ded46d82e32697046cf3ecaa63eb71b
Markus M. May <160079+triplem@users.noreply.github.com>
2021-04-28 21:05:30 +0200

    fix: compile (#54)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val kotlin_version: String = "1.4.32"
+
 plugins {
     // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
     `kotlin-dsl`
@@ -20,18 +22,18 @@ tasks.withType<KotlinCompile>().configureEach {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.4.30"))
-    implementation(kotlin("bom", "1.4.30"))
-    implementation(kotlin("reflect", "1.4.30"))
-    implementation(kotlin("stdlib-jdk8", "1.4.30"))
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.4.20") {
+    implementation(kotlin("gradle-plugin", kotlin_version))
+    implementation(kotlin("bom", kotlin_version))
+    implementation(kotlin("reflect", kotlin_version))
+    implementation(kotlin("stdlib-jdk8", kotlin_version))
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:$kotlin_version") {
         exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
     }
     implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.1.1")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.16.0") {
         exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
     }
-    implementation("org.owasp:dependency-check-gradle:6.1.0")
+    implementation("org.owasp:dependency-check-gradle:6.1.5")
     implementation("org.asciidoctor:asciidoctor-gradle-jvm:3.2.0")
     implementation("com.bmuschko:gradle-docker-plugin:6.7.0")
 }
