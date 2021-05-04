829debd8c5fad3cc2ffb67eae9c968699828548b
triplem <160079+triplem@users.noreply.github.com>
2021-05-04 21:31:39 +0200

    remove unnecessary class

--- modules/application-core/src/main/kotlin/org/javafreedom/kcd/application/port/output/repository/repositoryExceptions.kt
+++
@@ -1,6 +0,0 @@
-package org.javafreedom.kcd.application.port.output.repository
-
-/**
- * Exception thrown if Observation is not found in Repository using the given parameters
- */
-class ObservationNotFoundException(message: String) : RuntimeException(message)
--- modules/application-core/src/main/kotlin/org/javafreedom/kcd/domain/model/exceptions.kt
+++ modules/application-core/src/main/kotlin/org/javafreedom/kcd/domain/model/exceptions.kt
@@ -2,3 +2,8 @@ package org.javafreedom.kcd.domain.model
 
 open class BusinessException(message: String) : RuntimeException(message)
 class ReportException(message: String) : BusinessException(message)
+
+/**
+ * Exception thrown if Observation is not found in Repository using the given parameters
+ */
+class ObservationNotFoundException(message: String) : BusinessException(message)
